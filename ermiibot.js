966d099a5b1823b88e97d66acd59500149ab7438
Ermelber <ermelber@live.it>
2019-04-20 16:29:49 +0200

    idk

--- ermiibot.js
+++ ermiibot.js
@@ -33,30 +33,30 @@ client.on('message', message =>
 
     if (command.guildOnly && message.channel.type !== 'text') 
     {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
+			return message.reply('I can\'t execute that command inside DMs!');
+		}
 
     if (command.args && !args.length) 
     {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
+			let reply = `You didn't provide any arguments, ${message.author}!`;
 
         if (command.usage) 
         {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
+					reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+				}
 
-		return message.channel.send(reply);
-	}
+			return message.channel.send(reply);
+		}
 
     try 
     {
-		command.execute(message, args);
+			command.execute(message, args);
     } 
     catch (error) 
     {
-		console.error(error);
-		message.reply('There was an error trying to execute that command!');
-	}
+			console.error(error);
+			message.reply('There was an error trying to execute that command!');
+		}
 });
 
 client.login(token);
