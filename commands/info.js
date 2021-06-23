2c29c9a7eedac2330ec54e65853a39c1c56d9f8d
Ermelber <ermelber@live.it>
2021-06-23 10:43:37 +0200

    Add bot information configuration

--- commands/info.js
+++ commands/info.js
@@ -1,9 +1,11 @@
+const { description } = require('../settings.json');
+
 module.exports = {
     name: 'info',
     aliases: ['i'],
 	description: 'Information about this bot',
     execute(message, args)
     {
-		message.channel.send('This bot was made by Ermelber. Not Anymore Hosted on Szymmy\'s Pi.');
+		message.channel.send(description);
 	},
 };
